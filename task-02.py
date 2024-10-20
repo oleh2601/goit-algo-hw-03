c58d02b017f18f076097eedf15fa5c1766c0c354
dos_santos <nobackups@protonmail.com>
2024-10-20 10:59:43 +0300

    Update task-02.py

--- task-02.py
+++ task-02.py
@@ -1,11 +1,10 @@
-import random
+from random import randint
 
 
 def get_numbers_ticket(min, max, quantity):
     
     #validating data as per requirements
-    if (min < 1) or (min > 999) or (max < 2) or (max > 1000)\
-            or (quantity < min) or (quantity > max):
+    if (min < 1) or (min > 999) or (max < 2) or (max > 1000) or (min > max) or ((max-min+1) < quantity):
         return [] # Returning an empty list if the parameters are invalid as per task
     else:
 
@@ -14,7 +13,7 @@ def get_numbers_ticket(min, max, quantity):
 
         #running the loop until the set is fully filled
         while len(set_of_random_numbers) < quantity:
-            set_of_random_numbers.add(random.randint(min, max))
+            set_of_random_numbers.add(randint(min, max))
 
         #converting the set to a list and sorting the new list as per task 
         return sorted(list(set_of_random_numbers))
@@ -25,13 +24,13 @@ def get_numbers_ticket(min, max, quantity):
 
 #data entry, example from the task
 
-min = 1  #possible range 1 - 999
-max = 1000  #possible range 2 - 1000
-quantity = 10  #possible range min to nax 
+min = 10  #possible range 1 - 999
+max = 14  #possible range 2 - 1000
+quantity = 5 #no range 
 
 #getting the results using our function
 
 results = get_numbers_ticket(min, max, quantity)
 
 #output test
-#print(results)
+# print(results)
